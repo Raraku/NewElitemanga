fb3e711c51460e28fce81273a44ad7a4a543d920
raraku <airenov500@gmail.com>
2020-09-06 17:28:49 +0100

    work

--- users/models.py
+++ users/models.py
@@ -61,7 +61,7 @@ class Profile(models.Model):
     level = models.CharField(max_length=80, choices=LEVELS, default=LEVELS[0][0])
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(blank=True, null=True, upload_to="profile-avatars")
-    avatar_thumbnail = ImageField(
+    avatar_thumbnail = models.ImageField(
         upload_to="product-thumbnails", null=True, blank=True,
     )
     social_avatar = models.URLField(blank=True, null=True)
